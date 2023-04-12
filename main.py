4090cf27111417fae58051c8e66fc23ef497fa31
perinecz <perinecz.laszlo@gmail.com>
2023-04-12 09:18:07 +0200

    main.py elkészült

---
+++ main.py
@@ -0,0 +1,45 @@
+import math
+
+def szogFok(sz, m1, m2):
+    return math.degrees(math.acos((m1**2 + m2**2 - sz**2)/(2*m1*m2)))
+
+#0
+print("Háromszögek feldolgozása fájlokban.")
+
+#1
+finp = open("haromszogek.txt", mode="rt", encoding="utf-8")
+szoveg = finp.read()
+sorok = szoveg.split("\n")
+finp.close()
+
+#2
+szogekLines = []
+for item in sorok:
+    if item != '':
+        reszletek = item.split(';')
+        a = float(reszletek[0].replace(',' , '.'))
+        b = float(reszletek[1].replace(',' , '.'))
+        c = float(reszletek[2].replace(',' , '.'))
+
+        alfa = szogFok(a, b, c)
+        betta = szogFok(b, a, c)
+        gamma = szogFok(c, a, b)
+
+        #1. módszer
+        szogekSzovegesen = (str(alfa), str(betta), str(gamma))
+        szogekSor = ';'.join(szogekSzovegesen)
+        # 2. módszer
+        # szogekSor = f"{alfa:.2f};{betta:.2f};{gamma:.2f}\n"  !!!!!! '\n'
+        szogekLines.append(szogekSor)
+
+
+#3
+fout = open("szogek.txt", mode="wt", encoding="utf-8")
+
+#1. módszer
+tartalom = "\n".join(szogekLines)
+fout.write(tartalom)
+#2. módszer
+#fout.writelines(szogekLines)
+
+fout.close()
